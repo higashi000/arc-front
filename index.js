3495ae9cc03cf21dfb3453e0409e0cfcaf0b9818
higashi000 <higashi.ict@gmail.com>
2020-03-02 21:35:38 +0900

    Add data form

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
-let data = {text: "hogehoge", mention: [{slackrn: "ほげ", twitterid: "ふが"}, {slackrn: "ひがし", twitterid: "higashi136_2"}]};
-let method = "POST";
-let body = JSON.stringify(data);
-let headers = {
-  'Content-Type': 'application/json',
+function SendData() {
+  let data = {
+    text: document.getElementById("sendText").value,
+    mention: [],
+    channel: document.getElementById("channelID").value,
+  };
+
+  for (let i = 0; i < cntFormNum; ++i) {
+    let slackrnID = "slackrn" + String(i)
+    let twitterID = "twitterid" + String(i)
+    data["mention"].push({
+      slackrn: document.getElementById(slackrnID).value,
+      twitterid: document.getElementById(twitterID).value,
+    })
+  }
+
+  let method = "POST";
+  let body = JSON.stringify(data);
+  let headers = {
+    'Content-Type': 'application/json',
+  }
+
+  fetch("https://higashi-arc.appspot.com/arc/AddMsg", {
+    method,
+    headers,
+    body,
+  }).then(res => res.json()).catch(error => console.error('Error:', error));
 }
 
-fetch("https://higashi-arc.appspot.com/arc/AddMsg", {
-  method,
-  headers,
-  body,
-}).then(res => res.json()).catch(error => console.error('Error:', error));
+var cntFormNum = 0;
+function AddForm() {
+  console.log(cntFormNum);
+  var indentForm = '<div id = "languageIndnent' + String(cntFormNum) + '">'
+    + '<div class = "cp_iptxt">'
+    + '<p>Slack Real Name</p><input type="text" id = "slackrn' + String(cntFormNum) + '"/>'
+    + '</div>'
+    + '<div class = "cp_iptxt">'
+    + '<p>TwitterID</p><input type="text" id = "twitterid' + String(cntFormNum) + '"/>'
+    + '</div>'
+    + '</div>';
+
+  document.getElementById('User').insertAdjacentHTML('beforeend', indentForm);
+  cntFormNum += 1;
+}
