2ec394ec4c3410b866bc38d3fe19760ccacb69fe
higashi000 <higashi.ict@gmail.com>
2020-03-10 15:32:21 +0900

    Fix typo

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function SendData() {
     method,
     headers,
     body,
-    mode: 'no-cors'
+    mode: 'no-cors',
   }).then(res => res.json()).catch(error => console.error('Error:', error));
 }
 
